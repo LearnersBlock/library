fd564dccd856d7ddcdcdad59dd0f0dc5ff56a56f
maggie0002 <arran.magee@icloud.com>
2021-02-28 16:05:33 -0800

    Add Spanish language files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,11 @@
 import enUS from './enUS.json'
 import French from './fr.json'
 import Arabic from './ar.json'
+import Spanish from './es.json'
 // Here you can define additional languages
 export default {
   'en-us': enUS,
   'fr': French,
-  'ar': Arabic
+  'ar': Arabic,
+  'es': Spanish
 }
