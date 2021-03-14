5295c7b0b74aff3675082dba8135ef4f2990bb7f
maggie0002 <arran.magee@icloud.com>
2021-03-13 19:52:47 -0800

    Add logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,9 +13,7 @@
         />
 
         <q-avatar>
-          <span class="material-icons q-ml-md text-h5">
-          view_in_ar
-        </span>
+          <img src="../assets/lb-logo.png">
         </q-avatar>
 
         <q-toolbar-title class="josefin text-h5 q-mt-xs" >
