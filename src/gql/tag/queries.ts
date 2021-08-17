8c886ac68dbc9ff75346b71abc19a57f1115dd38
maggie0002 <arran.magee@icloud.com>
2021-08-17 16:39:40 -0700

    Show only used filters

--- src/gql/tag/queries.ts
+++ src/gql/tag/queries.ts
@@ -2,7 +2,9 @@ import gql from 'graphql-tag'
 
 export const GET_TAGS = gql`
   query tags{
-      tags(sort: "published_at:asc") {
+      tags(where: { 
+          resources: { name_contains: null }
+        }, sort: "published_at:asc") {
           id
           tag
       }
